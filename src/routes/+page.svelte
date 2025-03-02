09a2f064afdbb377c376fcf3a1f838eb00493259
slacktracer <slacktracer@gmail.com>
2025-03-02 13:46:22 -0300

    New page and ThreeJS.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to fire starter</h1>
+<p>Visit <a href="./shady">svelte.dev/docs/kit</a> to read the documentation</p>
